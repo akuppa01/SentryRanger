96f3c8d1d599efd24b0a483d933c97654976535a
Aditya Kuppa <akuppa01@gmail.com>
2022-07-08 14:30:46 -0500

    added code to process create and comments

--- SentryToRanger.py
+++ SentryToRanger.py
@@ -92,64 +92,89 @@ def parse_arg(arg):
     arr = arg.strip().lower().split(" ")
     # print(arr)
     try:
-        if len(arr) == 0 or len(arr) == 1:
+        if len(arr) == 0 or len(arr) == 1: # check if arg is an empty line 
+            print("check A")
             return
         
-        policyName = arr[-1].split(';')[0] # remove semicolons from name
-
-        # create new Policy class object and initialize
+        if arg[-2] != ';': # check if arg is a comment
+            # print("check B", arg[-2])
+            return
+        
         currentPolicy = Policy()
-        if policies.get(policyName) != None:
-            currentPolicy = policies[policyName]
-        else:
+        if arr[0] == "create" and arr[1] == 'role':
+            print("check 1")
+            currentPolicy = Policy()
+            policyName = arr[-1].split(';')[0]
             currentPolicy.set_policyName(policyName)
             policies[policyName] = currentPolicy
 
-        if arr[1] == 'select':
-            currentPolicy.grantType = 'select'
-        elif arr[1] == 'all':
-            currentPolicy.grantType = 'All'
-
+        elif arr[0] == "grant" and arr[1] == 'role': #assigning policy to a group
+            print("check 2")
+            # TODO: add precon to check if role exists beforehand??
+            policyName = arr[2]
+            group = arr[-1].split(';')[0]
+            if policies.get(policyName) != None:
+                currentPolicy = policies[policyName]
+            else:
+                currentPolicy.set_policyName(policyName)
+                policies[policyName] = currentPolicy
+            currentPolicy.groupName = group
         
-        if arr[2][0] == "(" and arr[3] == "on" and arr[4] == "table": # case[1] -> policy for column
-            # print("check 1")
-            currentPolicy.policyFor = "column"
-            currentPolicy.policyDescription = "create policy for " + currentPolicy.policyFor
-            currentPolicy.columnName = arr[2][1:-1] # remove parentheses
-            db_tb = arr[5].split(".")
-            currentPolicy.databaseName = db_tb[0] 
-            currentPolicy.tableName = db_tb[1]
+        else:
+            print("check 3")
+            policyName = arr[-1].split(';')[0] # remove semicolons from name
 
-        elif arr[2] == "on":
-            # print("check 2")
-            if arr[3] == "table": # case[2] -> policy for table
-                currentPolicy.policyFor = "table"
-                db_tb = arr[4].split(".")
+            # create new Policy class object and initialize
+            if policies.get(policyName) != None:
+                currentPolicy = policies[policyName]
+            else:
+                currentPolicy.set_policyName(policyName)
+                policies[policyName] = currentPolicy
+
+            if arr[1] == 'select':
+                currentPolicy.grantType = 'select'
+            elif arr[1] == 'all':
+                currentPolicy.grantType = 'All'
+
+            
+            if arr[2][0] == "(" and arr[3] == "on" and arr[4] == "table": # case[1] -> policy for column
+                # print("check 1")
+                currentPolicy.policyFor = "column"
+                currentPolicy.policyDescription = "create policy for " + currentPolicy.policyFor
+                currentPolicy.columnName = arr[2][1:-1] # remove parentheses
+                db_tb = arr[5].split(".")
                 currentPolicy.databaseName = db_tb[0] 
                 currentPolicy.tableName = db_tb[1]
-                
-            if arr[3] == "database": # case[3] -> policy for database
-                currentPolicy.policyFor = "database"
-                currentPolicy.databaseName = arr[4]
 
-            if arr[3] == "server": # case[4] -> policy for server
-                currentPolicy.policyFor = "server"
-                currentPolicy.serviceName = arr[4]
+            elif arr[2] == "on":
+                # print("check 2")
+                if arr[3] == "table": # case[2] -> policy for table
+                    currentPolicy.policyFor = "table"
+                    db_tb = arr[4].split(".")
+                    currentPolicy.databaseName = db_tb[0] 
+                    currentPolicy.tableName = db_tb[1]
+                    
+                if arr[3] == "database": # case[3] -> policy for database
+                    currentPolicy.policyFor = "database"
+                    currentPolicy.databaseName = arr[4]
 
-            currentPolicy.policyDescription = "create policy for " + currentPolicy.policyFor
+                if arr[3] == "server": # case[4] -> policy for server
+                    currentPolicy.policyFor = "server"
+                    currentPolicy.serviceName = arr[4]
 
-        currentPolicy.groupName = "BigData" #### TODO: maybe change it later
-
+                currentPolicy.policyDescription = "create policy for " + currentPolicy.policyFor
+        print("check end")
     except Exception as error:
         print("error", error)
     
 
 # Get command line arguments and print help
-def get_arguments():
+def get_arguments(inputfile):
     # INFO: to read stuff from command line, refer to Prasoon's original code and use argparse module
     args = []
-    with open('SentryCommands.txt') as f: # reading input file with commands
+    with open(inputfile) as f: # reading input file with commands
         args = f.readlines()
+    f.close()
     
 
     for i in range(7):#len(args)): # looping through each command
@@ -162,6 +187,8 @@ def get_arguments():
 
 # Start
 if __name__ == "__main__":
-    get_arguments()
+    inputfile = 'InputFiles/GroupAssign.txt'
+    get_arguments(inputfile)
+    print("policies len", len(policies))
     for i in policies:
         create_policy_json(policies[i])
--- makefile
+++ makefile
@@ -6,6 +6,7 @@ default:
 # post:
 # 	for file in TempFiles:
 # 		curl -ivk  -u 'admin:OracleTeamUSA!123' -X POST -H "Content-Type: application/json" -d @ll.json https://129.80.21.216:6182/service/public/v2/api/policy
+# curl -ivk  -u 'admin:OracleTeamUSA!123' -X POST -H "Content-Type: application/json" -d @role1_policy.json https://129.80.21.216:6182/service/public/v2/api/policy
 
 clean:
 	rm -f TempFiles/*.json
