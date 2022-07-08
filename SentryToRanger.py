0a4b982cb75008c1ca3ed6675446c49006e6e443
Aditya Kuppa <akuppa01@gmail.com>
2022-07-08 13:01:09 -0500

    creates individual json files for each policy

--- SentryToRanger.py
+++ SentryToRanger.py
@@ -2,130 +2,143 @@ import argparse
 import json
 from turtle import update
 
-def create_policy_json():
+policies = {}
 
-    jsonFile = open('TemplateFiles/createpolicy.json')
-    tmpJsonFile = open('TempFiles/tmpcreatepolicy.json', 'w')
-    jsonData = json.load(jsonFile)
-    newPolicyName = {"name": policyName,}
-    newServiceName = {"service": serviceName, }
-    newPolicyDescription = {"description": policyDescription, }
+class Policy:
 
-    newResource = {
-                        "resources": {
-                            "database": {
-                                "values": [databaseName],
-                                "isRecursive": False,
-                                "isExcludes": False
-                            },
-                            "table": {
-                                "values": [tableName],
-                                "isRecursive": False,
-                                "isExcludes": False
-                            },
-                            "column": {
-                                "values": [columnName],
-                                "isRecursive": False,
-                                "isExcludes": False
-                            }
-                        }
-                    }
-    newPolicyItems = {
-                        "policyItems": [
-                            {
-                                "accesses": [
-                                    {
-                                    "isAllowed": True, 
-                                    "type": grantType
-                                    }, 
-                                    {
-                                    "isAllowed": True, 
-                                    "type": "update"
-                                    }, 
-                                    {
-                                    "isAllowed": True, 
-                                    "type": "create"
+    def __init__(self):
+        self.policyName = "DUMMY"
+
+        self.columnName = "DUMMY"
+        self.tableName = "DUMMY"
+        self.databaseName = "DUMMY"
+        self.serviceName = "amoghadint_hive"
+
+        self.groupName = "DUMMY"
+        self.policyDescription = "DUMMY"
+        self.grantType = "DUMMY" # select/all
+        self.policyFor = "DUMMY" # for column/table/database/server
+        self.policyJSONfile = "DUMMY"
+        self.jsonFile = "DUMMY"
+        self.jsonData = "DUMMY"
+
+    def set_policyName(self,name):
+        self.policyName = name
+        self.jsonFile = open('TemplateFiles/createpolicy.json')
+        self.policyJSONfile = open('TempFiles/{}_policy.json'.format(name), 'w')
+        self.jsonData = json.load(self.jsonFile)           
+    
+
+def create_policy_json(policy):
+
+    # jsonFile = open('TemplateFiles/createpolicy.json')
+    # tmpJsonFile = open('TempFiles/tmpcreatepolicy.json', 'w')
+
+    policy.jsonData.update( {"name": policy.policyName,} )
+    policy.jsonData.update( {"service": policy.serviceName, } )
+    policy.jsonData.update( {"description": policy.policyDescription, } )
+
+    policy.jsonData.update( {
+                                "resources": {
+                                    "database": {
+                                        "values": [policy.databaseName],
+                                        "isRecursive": False,
+                                        "isExcludes": False
+                                    },
+                                    "table": {
+                                        "values": [policy.tableName],
+                                        "isRecursive": False,
+                                        "isExcludes": False
+                                    },
+                                    "column": {
+                                        "values": [policy.columnName],
+                                        "isRecursive": False,
+                                        "isExcludes": False
                                     }
-                                ], 
-                                "groups": [
-                                    groupName
+                                }
+                            } )
+    policy.jsonData.update( {
+                                "policyItems": [
+                                    {
+                                        "accesses": [
+                                            {
+                                            "isAllowed": True, 
+                                            "type": policy.grantType
+                                            }, 
+                                            {
+                                            "isAllowed": True, 
+                                            "type": "update"
+                                            }, 
+                                            {
+                                            "isAllowed": True, 
+                                            "type": "create"
+                                            }
+                                        ], 
+                                        "groups": [
+                                            policy.groupName
+                                        ]
+                                    }
                                 ]
-                            }
-                        ]
-                    }
-    jsonData.update(newPolicyDescription)
-    jsonData.update(newResource)
-    jsonData.update(newPolicyItems)
-    jsonData.update(newPolicyName)
-    jsonData.update(newServiceName)
-    json.dump(jsonData, tmpJsonFile, indent = 2)
-    jsonFile.close()
-    tmpJsonFile.close()
+                            } )
+
+    json.dump(policy.jsonData, policy.policyJSONfile, indent = 2)
+    policy.jsonFile.close()
+    policy.policyJSONfile.close()
 
 
 def parse_arg(arg):
     # TODO: add preconditions to check for valid commands
 
-    global serviceName
-    global policyName
-    global databaseName
-    global tableName
-    global columnName
-    global groupName
-    global policyDescription
-    global grantType
-    global policyFor
-
-    serviceName = "DUMMY"
-    policyName = "DUMMY"
-    databaseName = "DUMMY"
-    tableName = "DUMMY"
-    columnName = "DUMMY"
-    groupName = "DUMMY"
-    policyDescription = "DUMMY"
-    grantType = "DUMMY" # select/all
-    policyFor = "DUMMY" # for column/table/database/server
-
     arr = arg.strip().lower().split(" ")
     # print(arr)
     try:
-        if len(arr) == 0:
+        if len(arr) == 0 or len(arr) == 1:
             return
+        
+        policyName = arr[-1].split(';')[0] # remove semicolons from name
+
+        # create new Policy class object and initialize
+        currentPolicy = Policy()
+        if policies.get(policyName) != None:
+            currentPolicy = policies[policyName]
+        else:
+            currentPolicy.set_policyName(policyName)
+            policies[policyName] = currentPolicy
+
         if arr[1] == 'select':
-            grantType = 'select'
+            currentPolicy.grantType = 'select'
         elif arr[1] == 'all':
-            grantType = 'All'
+            currentPolicy.grantType = 'All'
 
         
         if arr[2][0] == "(" and arr[3] == "on" and arr[4] == "table": # case[1] -> policy for column
             # print("check 1")
-            policyFor = "column"
-            policyDescription = "create policy for " + policyFor
-            columnName = arr[2][1:-1] # remove parentheses
+            currentPolicy.policyFor = "column"
+            currentPolicy.policyDescription = "create policy for " + currentPolicy.policyFor
+            currentPolicy.columnName = arr[2][1:-1] # remove parentheses
             db_tb = arr[5].split(".")
-            databaseName = db_tb[0] 
-            tableName = db_tb[1]
+            currentPolicy.databaseName = db_tb[0] 
+            currentPolicy.tableName = db_tb[1]
 
         elif arr[2] == "on":
             # print("check 2")
             if arr[3] == "table": # case[2] -> policy for table
-                policyFor = "table"
+                currentPolicy.policyFor = "table"
                 db_tb = arr[4].split(".")
-                databaseName = db_tb[0] 
-                tableName = db_tb[1]
+                currentPolicy.databaseName = db_tb[0] 
+                currentPolicy.tableName = db_tb[1]
                 
             if arr[3] == "database": # case[3] -> policy for database
-                policyFor = "database"
-                databaseName = arr[4]
+                currentPolicy.policyFor = "database"
+                currentPolicy.databaseName = arr[4]
 
             if arr[3] == "server": # case[4] -> policy for server
-                policyFor = "server"
-                serviceName = arr[4]
+                currentPolicy.policyFor = "server"
+                currentPolicy.serviceName = arr[4]
 
-            policyDescription = "create policy for " + policyFor
+            currentPolicy.policyDescription = "create policy for " + currentPolicy.policyFor
 
-        policyName = arr[-1].split(';')[0] # remove semicolons from name
-        groupName = "BigData" # TODO: maybe change later
+        currentPolicy.groupName = "BigData" #### TODO: maybe change it later
 
     except Exception as error:
         print("error", error)
@@ -139,16 +152,16 @@ def get_arguments():
         args = f.readlines()
     
 
-    # count = 0
-    # for arg in args: # looping through each command
-    #     count += 1
-    #     print('arg {}: {}'.format(count,arg))    
-    #     parse_arg(arg)
+    for i in range(7):#len(args)): # looping through each command
+        arg = args[i]
+        print('arg {}: {}'.format(i+1,arg))    
+        parse_arg(arg)
 
-    parse_arg(args[0])
+    # parse_arg(args[0])
 
 
 # Start
 if __name__ == "__main__":
     get_arguments()
-    create_policy_json()
+    for i in policies:
+        create_policy_json(policies[i])
---
+++ makefile
@@ -0,0 +1,11 @@
+
+
+default:
+	python SentryToRanger.py
+
+# post:
+# 	for file in TempFiles:
+# 		curl -ivk  -u 'admin:OracleTeamUSA!123' -X POST -H "Content-Type: application/json" -d @ll.json https://129.80.21.216:6182/service/public/v2/api/policy
+
+clean:
+	rm -f TempFiles/*.json
