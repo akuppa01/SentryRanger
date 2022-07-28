f9b52d1c99f9c6277fd981e4235abd8fbf7057ca
Aditya Kuppa <akuppa01@gmail.com>
2022-07-28 13:48:33 -0500

    working code

--- SentryToRanger.py
+++ SentryToRanger.py
@@ -1,50 +1,46 @@
 import argparse
 import json
-import subprocess
 import os
+import logging
 from turtle import update
 
-HOST_NUMBER = '129.80.66.56:6182'
 
-roles = {}
-resources = {}
-jsonFiles = {}
-errorResources = {}
+RANGER_ADMIN_HOST = '129.80.66.56:6182'
+SERVICE_NAME = "kkrkdonotd_hive"
+
+roles = {}     # dict of {"role name" : role object}
+resources = {} # dict of {"resource name" : resource object}
+jsonFiles = {} # dict of {"resource name" : json FILE}
+errorResources = {} # dict of {"resource name" : resource object} for those policies without any assigned group
 
 """
 Any Ranger Policy at minimum needs
 
-policyName, serverName, databaseName, 1 permission, 1 group
+policyName, serviceName, database/URI name, 1 permission, 1 group
 
 """
 class Resource:
+    """Resource class stores key information for each policy"""
     def __init__(self): 
         self.resourceStr = "DUMMY"
         self.columnName = "*"
         self.tableName = "*"
         self.databaseName = "*"
         self.uriName = "not set"
-        self.serverName = "kkrkdonotd_hive" ## TODO: change it to parameter
-                                            ## TODO: check to see if this is same as 'service' in json file
+        self.serviceName =  SERVICE_NAME ## TODO: change it to parameter
+                                         ## TODO: what to do if a command asks for server change
         self.isURI = False
         self.description = "description TBD"
-        self.labels = {}  # { 'role name' : [ 'select' , 'All' ] }
-        self.policyJSONfile = "DUMMY"
-        self.jsonFile = "DUMMY"
-        self.jsonData = "DUMMY"
+        self.labels = {}  # { 'role name' : set('select' , 'All') ,}
 
-        self.jsonFile = open('TemplateFiles/createpolicy.json')
-        self.policyJSONfile = "empty right now"
-        self.jsonData = json.load(self.jsonFile) 
 
 
 class Role:
+    """Role class stores key information for each role"""
     def __init__(self): ## TODO: account for cases where DUMMY has not been changed/re-assigned
         self.roleName = "DUMMY"
         self.resources = set()
         self.groups = set()
-        # self.policyDescription = []
-        # self.permissions = []  # select/All/Read/Write etc.
         self.roleFor = "DEFAULT" # for column/table/database/server
 
     def set_roleName(self,name):
@@ -53,32 +49,34 @@ class Role:
     def print_policy(self):
         print ("Role: " + self.roleName)
         print ("   roleFor: " + self.roleFor)
-        # print ("   resourceStr: " + self.resources)
-        # print ("   permissions: {}".format(self.permissions))
         print ("   groups: {}".format(self.groups))       
     
 
 def create_policy_json(policy):
-    # print(policy)
+    """
+    Given a policy parameter, this func creates corresponding json file and adds it to TempFiles directory
+    if function returns None, it means json FILE was not created
+    if function returns True, json FILE was created successfully
+    """
+    
     jsonFile = open('TemplateFiles/createpolicy.json')
     jsonData = json.load(jsonFile) 
     
-  
     res = getPolicyItems(policy)
     policyItems = res[0]
     policyLabels = res[1]
 
     if len(policyLabels) == 0:
-        print("error:", "no groups assigned to any of the roles in this resource =>", policy.resourceStr)
+        logging.error("no groups assigned to any of the roles in this resource => " + policy.resourceStr)
         errorResources[policy.resourceStr] = policy
         return None
     
     policy.description = str(policyLabels)
     jsonData.update( {"name": policy.resourceStr, } )
-    jsonData.update( {"service": policy.serverName, } )
+    jsonData.update( {"service": policy.serviceName, } )
     jsonData.update( {"description": policy.description, } )
 
-    if policy.uriName != "not set":
+    if policy.uriName != "not set": # this policy is for a URL
         jsonData.update( {
                                 "resources": {
                                     "url": {
@@ -88,7 +86,7 @@ def create_policy_json(policy):
                                     }
                                 },
                          } )
-    else: 
+    else: # this policy is not for a URL
         jsonData.update( {
                                 "resources": {
                                     "database": {
@@ -109,12 +107,8 @@ def create_policy_json(policy):
                                 }
                             } )
     
-    jsonData.update( {
-                                "policyItems": policyItems
-                            } )
-    jsonData.update( {
-                                "policyLabels": policyLabels
-                            } )
+    jsonData.update( { "policyItems": policyItems, } )
+    jsonData.update( { "policyLabels": policyLabels, } )
 
     policyJSONfile = open('TempFiles/{}.json'.format(policy.resourceStr), 'w')
     json.dump(jsonData, policyJSONfile, indent = 2)
@@ -129,10 +123,7 @@ def getPolicyItems(policy):
     for role in policy.labels:
         if len(roles[role].groups) != 0:
             policyLabels.append(role)
-            # for group in groups, add to groups array
-            # for permissions in policy.labels[label], create a permission dict
             accesses = []
-            # print("drake",policy.resourceStr, policy.labels[role], role)
             for permission in policy.labels[role]:
                 accesses.append( {
                                     "isAllowed": True, 
@@ -153,26 +144,25 @@ def parse_arg(arg):
     arr = arg.strip().lower().split(" ")
 
     # print("\n")
-    print(arr)
+    # print(arr)
     # print("\n")
     try:
         if len(arr) == 0 or len(arr) == 1: # check if arg is an empty line 
-            print("check A")
+            # print("check A")
             return
         
         if arg.rstrip()[-1] != ';': # check if arg is a comment
-            print("check B", arg.rstrip()[-1])
+            # print("check B", arg.rstrip()[-1])
             return
         
         
         currentRole = Role()
         if arr[0] == "create" and arr[1] == 'role': ## ignore this command maybe??
-            print("warning :", "encountered a 'create role' command")
+            logging.warning("encountered a 'create role' command for " + arr[2] )
             return
 
         if arr[0] == "grant" and arr[1] == 'role': # assigning policy to a group
-            print("check 2") ### HERE
-            # TODO: add precon to check if role exists beforehand??
+            # print("check 2") 
             currentRole.roleName = arr[2]
             group = arr[-1].split(';')[0]
           
@@ -186,17 +176,17 @@ def parse_arg(arg):
 
         else: # normal grant command
             ## TODO: add precon / check for spelling error
-            print("check 3")
+            # print("check 3")
             currentResource = Resource()
             argPermission = parse_Resource(currentResource, currentRole, arr)
             if argPermission != None:
                 createResource(currentResource, currentRole, argPermission)
             else:
-                print("WARNING:", "Encountered a SERVER related command")
+                logging.warning("encountered a SERVER related command")
 
         # print("check end")
     except Exception as error:
-        print("error", error)
+        logging.error(error)
     
 
 def parse_Resource(currentResource, currentRole, arr):
@@ -214,7 +204,7 @@ def parse_Resource(currentResource, currentRole, arr):
         argPermission = arr[1].capitalize()
 
     
-    currentResource.labels = {argRole : set([argPermission])} # maybe not needed
+    currentResource.labels = {argRole : set([argPermission])} 
 
     ## parsing col,tb,db,server
     if arr[2][0] == "(" and arr[3] == "on" and arr[4] == "table": # case[1] -> policy for column
@@ -240,11 +230,11 @@ def parse_Resource(currentResource, currentRole, arr):
         elif arr[3] == "server": # case[4] -> policy for server
             ## DOUBT... what to do if we encounter this case????
             currentRole.roleFor = "server"
-            # currentResource.serverName = arr[4]
+            # currentResource.serviceName = arr[4]
             return None
         
         elif arr[3] == "uri": # case[4] -> policy for server
-            ## DOUBT... what to do if we encounter this case????
+            ## DOUBT... what to do if we encounter this case???? how to parameterize instead of hard coding
             currentRole.roleFor = "uri"
             currentResource.uriName = arr[4][1:-1]
     
@@ -253,19 +243,20 @@ def parse_Resource(currentResource, currentRole, arr):
             
 def createResource(currentResource, currentRole, argPermission):
     resourceStr = "{}.{}.{}".format(currentResource.databaseName,currentResource.tableName,currentResource.columnName)
+    resourceStr = resourceStr.replace('*','all')
     # print(resourceStr)
-    print(currentRole.roleFor)
+    # print(currentRole.roleFor)
     if currentRole.roleFor == "uri":
         resourceStr = currentResource.uriName.replace("/", "-")[1:]
         currentResource.isURI = True
-        print("nai")
-    print("check 4") 
+        # print("nai")
+    # print("check 4") 
     currentResource.resourceStr = resourceStr
     roleName = currentRole.roleName
 
     #### by this point, labels have been updated
     if resources.get(resourceStr) != None: # existing resource
-        print("check 4.1")
+        # print("check 4.1")
         origResource = resources[resourceStr]
         # { 'role name' : [ 'select' , 'All' ] }
         if origResource.labels.get(roleName) != None: # role exists in original resource's label
@@ -283,7 +274,7 @@ def createResource(currentResource, currentRole, argPermission):
     # print("*** policyName",policyName, policies.get(policyName) != None)
 
     if roles.get(roleName) == None: # new role
-        print("check 4.2")        
+        # print("check 4.2")        
         roles[roleName] = currentRole
     
 
@@ -298,8 +289,8 @@ def get_arguments(INPUT_FILE):
     # aaa
     for i in range(0,len(args)): # looping through each command
         arg = args[i]
-        print("\n")
-        print('arg {}: {}'.format(i+1,arg))    
+        # print("\n")
+        # print('arg {}: {}'.format(i+1,arg))    
         parse_arg(arg)
 
     # parse_arg(args[0])
@@ -316,22 +307,22 @@ def updateRanger(policy):
     # GET_COMMAND = "curl -ivk  -u 'admin:OracleTeamUSA!123' -H \"Content-Type: application/json\" -X GET \"https://129.80.66.56:6182/service/public/v2/api/policy?policyName={}\"".format(policyName)
     # print("updateRanger; check 1")
     try:
-        # UPDATE_COMMAND = "curl -ivk -u 'admin:OracleTeamUSA!123' -X PUT -H \"Content-Type: application/json\" -d @TempFiles/" + policyJSONname + " https://" + HOST_NUMBER + "/service/public/v2/api/service/{\"" + policy.serverName + "\"}/policy/{\"" + policyName + "\"}/"
+        # UPDATE_COMMAND = "curl -ivk -u 'admin:OracleTeamUSA!123' -X PUT -H \"Content-Type: application/json\" -d @TempFiles/" + policyJSONname + " https://" + RANGER_ADMIN_HOST + "/service/public/v2/api/service/{\"" + policy.serviceName + "\"}/policy/{\"" + policyName + "\"}/"
         # print(UPDATE_COMMAND)
         # output = os.popen(GET_COMMAND).read()
         # gg = output.split()
         # print("updateRanger; check 2")
         # if (gg[-2]=='Not' and gg[-1]=='found'): #CREATE command
         # if (gg[-1] == "[]"):
-        print("createRanger; check 3", policy.resourceStr)
-        CREATE_COMMAND = "curl -ivk  -u 'admin:OracleTeamUSA!123' -X POST -H \"Content-Type: application/json\" -d @TempFiles/\"{}\" https://{}/service/public/v2/api/policy/".format(policyJSONname, HOST_NUMBER)    
+        # print("createRanger; check 3", policy.resourceStr)
+        CREATE_COMMAND = "curl -ivk  -u 'admin:OracleTeamUSA!123' -X POST -H \"Content-Type: application/json\" -d @TempFiles/\"{}\" https://{}/service/public/v2/api/policy/".format(policyJSONname, RANGER_ADMIN_HOST)    
         output = os.popen(CREATE_COMMAND).read()
-        print(CREATE_COMMAND)
+        # print(CREATE_COMMAND)
 
             # gg = output.split()
 
         # TODO: add another if statement to check that policy was added successfully
-        print("-------")
+        # print("-------")
         # print(gg)
         # print(gg[-2],gg[-1])
     except Exception as error:
@@ -343,25 +334,24 @@ def updateRanger(policy):
 # Start
 if __name__ == "__main__":
     INPUT_FILE = 'InputFiles/scaj-INPUT-FILE.txt'
-    # HOST_NUMBER = '129.80.66.56:6182'
-    # inputfile = 'InputFiles/GroupAssign.txt'
+
     get_arguments(INPUT_FILE)
-    print("roles len", len(roles))
-    print("roles =", roles.keys())
-    for i in roles:
-        print(i, roles[i].groups)
-    print("")
-    print("resources =")
+    # print("roles len", len(roles))
+    # print("roles =", roles.keys())
+    # for i in roles:
+    #     print(i, roles[i].groups)
+    # print("")
+    # print("resources =")
     # i = "d1.nitish_column.id"
     for i in resources:
-        print(i, resources[i].labels)
+        # print(i, resources[i].labels)
         cpj = create_policy_json(resources[i])
-        if cpj:
-            updateRanger(resources[i])
+        # if cpj:
+        #     updateRanger(resources[i])
 
 
-    print("\n")
-    print(errorResources.keys())
+    # print("\n")
+    # print(errorResources.keys())
     # for i in policies:
     
     # print(policies["admin_role"].policyName)
